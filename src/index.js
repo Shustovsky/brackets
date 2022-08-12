2b22f0ee914933bad5a5fd895b4c867a2c36804c
A. Shustovsky <a.shustovsky@gmail.com>
2022-08-12 11:28:20 +0400

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check = (str, bracketsConfig) => {
+    let arr = str.split('');
+
+
+    check = (arr) => {
+        for (i = 0; i < arr.length; i++) {
+            // console.log(`arr[i] === ${arr[i]}`);
+            for (j = 0; j < bracketsConfig.length; j++) {
+
+                if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) {
+                    // console.log(arr[i]);
+                    console.log(arr.splice(i, 2));
+                    // arr.splice(i, 2);
+                    check(arr)
+                }
+            }
+        }
+    }
+    check(arr);
+    return arr.length === 0 ? true : false;
 }
+
+/* check('()', [
+        ['(', ')']
+    ]) // -> true */
+/* check('())(', [
+        ['(', ')']
+    ]) // -> false */
+check('([{([()])}])', [
+        ['(', ')'],
+        ['[', ']'],
+        ['{', '}']
+    ]) // -> true
